0bed7a6c3dd244eb7bba2635f196d6e8ca0883ae
KostiaTrepyk <trepyk.kostiantynn@gmail.com>
2023-06-26 22:43:29 +0200

    added build version

---
+++ build/static/js/136.51baeed1.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkclient=self.webpackChunkclient||[]).push([[136],{2136:function(e,n,c){c.r(n);var t=c(184);n.default=function(){return(0,t.jsx)(t.Fragment,{children:(0,t.jsx)("div",{children:"ProfilePage component"})})}}}]);
+//# sourceMappingURL=136.51baeed1.chunk.js.map
